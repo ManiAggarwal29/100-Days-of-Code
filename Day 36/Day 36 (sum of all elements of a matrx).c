4c276bfa29428388b417a2cb2706e4ac2f53c6c9
ManiAggarwal29 <maniaggarwal29@gmail.com>
2025-11-11 21:57:04 +0530

    Add files via upload

---
+++ Day 36/Day 36 (sum of all elements of a matrx).c
@@ -0,0 +1,39 @@
+/* Day 36 Qiestion-72
+Find the sum of all elemets of a matrix.
+*/
+#include<stdio.h>
+int main(){
+    //n is the total number of rows
+    int n;
+    printf("enter the number of rows for array: ");
+    scanf("%d",&n);
+    //m is the total number of columns
+    int m;
+    printf("enter the number of columns for array: ");
+    scanf("%d",&m);
+    //Making a two dimensional array
+    int array[n][m];
+    //Entering and saving the elements in 2-D array
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            printf("Enter the array elements [%d][%d]\n",i,j);
+            scanf("%d",&array[i][j]);
+        }
+    }
+    //Printing the elements of 2-D array
+    printf("the Array is: \n");
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            printf("%d\t",array[i][j]);
+        }
+        printf("\n");
+    }
+    int sum=0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            sum=sum+array[i][j];;
+        }
+    }
+    printf("The sum of all elements of matrix: %d",sum);
+    return 0;
+}
