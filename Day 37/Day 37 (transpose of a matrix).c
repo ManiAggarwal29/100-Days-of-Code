e156f389409e3580c1c732dbae4ed604e4e01bf0
ManiAggarwal29 <maniaggarwal29@gmail.com>
2025-11-13 23:18:58 +0530

    Add files via upload

---
+++ Day 37/Day 37 (transpose of a matrix).c
@@ -0,0 +1,40 @@
+/* Day 37 Qiestion-74
+Find the transpose of a matrix.
+*/
+#include<stdio.h>
+int main(){
+    //n is the total number of rows
+    int n;
+    printf("enter the number of rows for array: ");
+    scanf("%d",&n);
+    //m is the total number of columns
+    int m;
+    printf("enter the number of columns for array: ");
+    scanf("%d",&m);
+    //Making a two dimensional array
+    int array[n][m];
+    //Entering and saving the elements in 2-D array
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            printf("Enter the array elements [%d][%d]\n",i,j);
+            scanf("%d",&array[i][j]);
+        }
+    }
+    //Printing the elements of 2-D array
+    printf("the Array is: \n");
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            printf("%d\t",array[i][j]);
+        }
+        printf("\n");
+    }
+    //Transpose of a matrix
+    printf("The transpose of a matrix:\n");
+    for(int j=0;j<m;j++){
+        for(int i=0;i<n;i++){
+            printf("%d\t",array[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
